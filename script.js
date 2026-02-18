085e50d7baf0004ce387f693d5c40f7296803df3
magrethmapinda03-prog <git addmagrethmapinda03@gmail.com>
2026-02-19 00:59:08 +0300

    Add medication adherence percentage summary

--- script.js
+++ script.js
@@ -2,22 +2,36 @@ const form = document.getElementById("medicineForm");
 const medicineList = document.getElementById("medicineList");
 let medicines = JSON.parse(localStorage.getItem("medicines")) || [];
 
-function displayMedicines() {
+ function displayMedicines() {
     medicineList.innerHTML = "";
 
     medicines.forEach((med, index) => {
         const li = document.createElement("li");
-        li.textContent = med.name + " - " + med.dosage;
+
+        li.innerHTML = `
+            ${med.name} - ${med.dosage} 
+            <button onclick="markAsTaken(${index})">
+                ${med.taken ? "Taken ✅" : "Mark as Taken"}
+            </button>
+        `;
+
         medicineList.appendChild(li);
     });
 }
+function markAsTaken(index) {
+    medicines[index].taken = true;
+    localStorage.setItem("medicines", JSON.stringify(medicines));
+    displayMedicines();
+}
+
+
 form.addEventListener("submit", function(event) {
     event.preventDefault();
 
     const name = document.getElementById("medicineName").value;
     const dosage = document.getElementById("dosage").value;
 
-    medicines.push({ name: name, dosage: dosage });
+    medicines.push({ name: name, dosage: dosage,taken: false });
     localStorage.setItem("medicines", JSON.stringify(medicines));
 
     form.reset();
